f60b919bb0d54585e494012527dba058f7111f14
Next <110189996+SpreestDev@users.noreply.github.com>
2022-11-07 21:58:43 +0100

    Add files via upload

--- JsonFile/src/main/kotlin/Main.kt
+++ JsonFile/src/main/kotlin/Main.kt
@@ -1,47 +1,7 @@
-import org.json.JSONObject
-import java.io.Serializable
-import java.nio.file.Files
-import java.nio.file.Path
-
 fun main(args: Array<String>) {
-    val path = "C:\\Users\\ciaoc\\OneDrive\\untitled3\\src\\main\\resources\\test.json"
-    val json = JSONObject()
+    println("Hello World!")
 
-    var player = Player("NextDev")
-
-    if(!Files.exists(Path.of("C:\\Users\\ciaoc\\OneDrive\\untitled3\\src\\main\\resources\\data"))) {
-        Files.createDirectory(Path.of("C:\\Users\\ciaoc\\OneDrive\\untitled3\\src\\main\\resources\\data"))
-    }
-
-    val file = JsonFile("C:\\Users\\ciaoc\\OneDrive\\untitled3\\src\\main\\resources\\data", player.address)
-    val map = mapOf(("player2" to player), ("bool2" to true))
-
-    file.reload()
-
-    player = file.getObject("player2", Player::class.java) as Player
-    println(player.address)
-    println(player.name)
-
-    //file.create(map as HashMap<String, Any>)
-
-    val file2 = JsonFile("C:\\Users\\ciaoc\\OneDrive\\untitled3\\src\\main\\resources\\data\\file")
-    val defaults = hashMapOf<String, Any>(
-        "a" to "1",
-        "b" to "2",
-        "c" to "3",
-        "d" to "4",
-        "e" to "5",
-        "f" to "6"
-    )
-
-    if (!file2.exists()) {
-        file2.create(defaults)
-    }
-
-    println(file2["a"])
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
 }
-
-class Player(var name: String):Serializable {
-    var address: String = "95.234.208.189"
-}
-
